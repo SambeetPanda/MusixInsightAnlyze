c0198bb1c95cb2fd219cccdbf0da448d62521047
SambeetPanda <112571281+SambeetPanda@users.noreply.github.com>
2025-09-25 02:01:43 +0530

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,11 @@
+# import kagglehub
+
+# # Download latest version
+# path = kagglehub.dataset_download("zaheenhamidani/ultimate-spotify-tracks-db")
+# print("Path to dataset files:", r"C:\Users\KIIT\Desktop\BEST\OTT analysis\dataset.py")
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("leonardopena/top50spotify2019")
+
+print("Path to dataset files:", path)
